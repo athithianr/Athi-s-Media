6446ea4c2353c06b135e12f3e10d235cc08c3d13
Athi Rajkumar <athithianr@gmail.com>
2019-06-16 22:02:03 -0400

    Implemented connection to Rest API

--- Athi's Media/popup.js
+++ Athi's Media/popup.js
@@ -1,31 +1,38 @@
-function createNode(element)
-{
-    return document.createElement(element);
+var request = new XMLHttpRequest
+//debugger;
+const QUOTE_API_URL = 'https://quotes.rest/qod';
+let h = new Headers()
+h.append('Accept', 'application/json')
+let req = new Request(QUOTE_API_URL, {
+    method: 'GET',
+    headers: h,
+});
 
+var quote;
+var author;
+
+fetch(req)
+    .then( (response)=>{
+        if(response.ok){
+            return response.json();
+        }else{
+            throw new Error("Bad HTTP stuff");
+        }
+    })
+    .then( (jsonData) =>{  
+        
+    })
+    .catch( (err) =>{
+        console.log('ERROR:', err.message)
+    });
+
+function simple(){
+    quote = jsonData.contents.quotes[0].quote
+    author = jsonData.contents.quotes[0].author
+    document.write(quote + " - " + author)
 }
 
-function append(parent, el)
-{
-    return parent.appendChild(el);
-
-}
-
-const ul= document.getElementById("Quotes");
-
-const url = 'https://rapidapi.com/andruxnet/api/random-famous-quotes';
-
-fetch(url)
-.then((resp) => resp.json())
-.then(function(data)){
-
-    let quotes = data.results;
-
-    return quotes.map(function(author)){
-
-    let li = createNode('li'),
-
-
-    }
-
+function simple1(){
+    var hello = "hello"
 
 }
